b11a3dbd82d249b2b4c7ee48a9832a4c11bede35
ivillamizar5 <ivan.151867@gmail.com>
2025-04-20 19:06:32 -0500

    Actualiza archivo: usuario/registro.js

--- usuario/registro.js
+++ usuario/registro.js
@@ -1,38 +1,19 @@
-
 import { validarUsuario } from "./funcionUsuario.js";
 import { cerrarSesion } from "./sesiones.js";
 
-const usuario = document.getElementById("usuario")
-const password = document.getElementById("password")
+const usuario = document.getElementById("usuario");
+const password = document.getElementById("password");
 const btnregistro = document.querySelector(".login-form"); 
-
 const cerrar = document.querySelector("#cerrarsesion");
-let ruta = "./index.html"
+let ruta = "./login.html";
 
-cerrarSesion(ruta,cerrar)
+cerrarSesion(ruta, cerrar);
 
 btnregistro.addEventListener("submit", async (e) => {
     e.preventDefault();
-
-    let valido = await validarUsuario(usuario.value, password.value)
-  
-    if(valido.length === 0){
-        console.log("aca")
-       
+    const success = await validarUsuario(usuario.value, password.value);
+    if (!success) {
+        console.log("No se pudo registrar el usuario");
+        // Optionally show an error message to the user
     }
-   
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
