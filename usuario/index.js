c9e4d57a4e2033bce5ca5e3be18cafac3f14893f
ivillamizar5 <ivan.151867@gmail.com>
2025-04-20 19:06:30 -0500

    Actualiza archivo: usuario/index.js

--- usuario/index.js
+++ usuario/index.js
@@ -1,18 +1,21 @@
 import { validarUsuarioInicio } from "./funcionUsuario.js";
 import { cerrarSesion } from "./sesiones.js";
 
-
 const cerrar = document.querySelector("#cerrarsesion");
-let ruta = "./index.html"
-cerrarSesion(ruta,cerrar)
+let ruta = "./index.html";
+cerrarSesion(ruta, cerrar);
 
-const usuario = document.getElementById("usuario")
-const password = document.getElementById("password")
-const btnregistro = document.querySelector(".login-form"); 
-
+const usuario = document.getElementById("usuario");
+const password = document.getElementById("password");
+const btnregistro = document.querySelector(".login-form");
+const errorMessage = document.getElementById("error-message");
 
 btnregistro.addEventListener("submit", async (e) => {
     e.preventDefault();
-    console.log("aca")
-    await validarUsuarioInicio(usuario.value, password.value)
-})
+    errorMessage.classList.add("hidden");
+    const success = await validarUsuarioInicio(usuario.value, password.value);
+    if (!success) {
+        errorMessage.textContent = "Usuario no encontrado o contraseña incorrecta";
+        errorMessage.classList.remove("hidden");
+    }
+});
